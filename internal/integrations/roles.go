251f3a74fbfeb5d9486d8bb6f5e375fc398b2ebf
Kent <utterback.kent.a@gmail.com>
2026-06-11 15:53:57 -0400

    feat: add role and integration-type constants to integrations package

---
+++ internal/integrations/roles.go
@@ -0,0 +1,22 @@
+package integrations
+
+const (
+	ResourceRoleManager    = "manager"
+	ResourceRoleRuntime    = "runtime"
+	ResourceRoleTool       = "tool"
+	ResourceRoleRemote     = "remote"
+	ResourceRoleFilesystem = "filesystem"
+)
+
+const (
+	TransponderRoleFile     = "file"
+	TransponderRoleEnv      = "env"
+	TransponderRoleKeychain = "keychain"
+	TransponderRoleVault    = "vault"
+	TransponderRoleAgent    = "agent"
+)
+
+const (
+	IntegrationTypeResource    = "resource"
+	IntegrationTypeTransponder = "transponder"
+)
